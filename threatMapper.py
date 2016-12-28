2b77cee4f87d9a1e2ab5fd35dc7b5e4f16d51ee4
VISHNU RAJU <rajuvishnu52@gmail.com>
2016-12-28 17:43:47 +0530

    Look up IPs against known IoC list

---
+++ threatMapper.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import requests,json
+ip_file = raw_input("Enter the input suspicious IP file : ")
+print "\n[*] Collecting data. Please wait ... "
+with open(ip_file, "r") as f:
+	for line in f:
+		ip_in = str(line)
+		resp = requests.get("https://www.threatcrowd.org/searchApi/v2/ip/report/?", {"ip": ip_in}).text
+		op = json.loads(resp)
+		op_code = op['response_code']
+		count = 0
+		if (op_code == "1"):
+			print "\n[*] IoC found for IP %s" %(ip_in)
+			print "[*] Suspicious domains found are : "
+			for k in op['resolutions']:
+				print "[-]"+ " " +op['resolutions'][count]['domain']
+				count = count+1
+			print "\n[*] Threat Map shall be found here - %s" %str(op['permalink'])
+f.close()
