8c6c36d72490783301f6c120117a9330ac861997
root <root@Nest>
2017-08-03 06:54:37 -0400

    Resolved formatting issues

--- TH_Recon.py
+++ TH_Recon.py
@@ -2,7 +2,7 @@ from ipwhois import IPWhois
 from pprint import pprint
 import csv
 with open("Whois_Output.csv","wb") as csvfile:
-	writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+	writer = csv.writer(csvfile, delimiter='~', quoting=csv.QUOTE_MINIMAL)
 	writer.writerow(['IP Address', 'Org Name', 'Org Country', 'Org Address', 'Description'])
 	with open("iplist.txt","r") as f:
 		iplst = f.readlines()
@@ -13,8 +13,11 @@ with open("Whois_Output.csv","wb") as csvfile:
 			country = r['asn_country_code']
 			name = r['nets'][0]['name']
 			ad = r['nets'][0]['address']
+			add =  ad.rstrip('\n').split('\n')
+			print add
 			desc= r['nets'][0]['description']
-			writer.writerow([ip, name, country, ad, desc])
+			des =  str(desc.rstrip('\n').split('\n'))
+			writer.writerow([ip, name, country, add, des])
 	f.close()
 csvfile.close()
 		
