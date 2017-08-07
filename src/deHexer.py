7e06f8c8084a8dd9c0aab48aee25f684e5626407
dh4wk <dh4wk@Nest>
2017-08-07 14:50:40 -0400

    De-obfuscation of hex coded files.

---
+++ src/deHexer.py
@@ -0,0 +1,22 @@
+import re
+pth = raw_input("Enter the obfuscated file - ")
+with open(pth,'r') as f:
+    with open("final.txt", "a") as w:
+        for line in f:
+            for word in line.split(';'):
+                try:
+                    if re.match(r'^\\x', word):
+                        print "word " + word
+                        word1 = word.replace('\\x', '')
+                        deobs = word1.decode('hex')
+                        print "deob " + deobs
+                        w.write(deobs)
+                    else:
+                        w.write(word)
+                except Exception,e:
+                    print str(e)
+                    w.write(word)
+                    pass
+                    
+
+            
