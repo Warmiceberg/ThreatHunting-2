2bdd3a54c665b19cfe7c5376fc2b353781c3f34c
root <root@Nest>
2017-08-11 03:01:55 -0400

    Fix and Additions

--- src/TH_Recon.py
+++ src/TH_Recon.py
@@ -8,16 +8,20 @@ with open("Whois_Output.csv","wb") as csvfile:
 	with open(pth,"r") as f:
 		iplst = f.readlines()
 		for ip in iplst:
-			print ip.strip()	
-			obj = IPWhois(ip.strip())
-			r = obj.lookup_whois()
-			country = r['asn_country_code']
-			name = r['nets'][0]['name']
-			ad = r['nets'][0]['address']
-			add =  ad.rstrip('\n').split('\n')
-			desc= r['nets'][0]['description']
-			des =  str(desc.rstrip('\n').split('\n'))
-			writer.writerow([ip, name, country, add, des])
+			try:
+				print ip.strip()	
+				obj = IPWhois(ip.strip())
+				r = obj.lookup_whois()
+				country = r['asn_country_code']
+				name = r['nets'][0]['name']
+				ad = r['nets'][0]['address']
+				add =  ad.rstrip('\n').split('\n')
+				desc= r['nets'][0]['description']
+				des =  str(desc.rstrip('\n').split('\n'))
+				writer.writerow([ip, name, country, add, des])
+			except:
+				print "Couldn't find details for IP : "+ip
+				pass
 	f.close()
 csvfile.close()
 		
--- src/screenGrabber.py
+++ src/screenGrabber.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from depot.manager import DepotManager
 count = 0
-pth = raw_input("Enter the path of the domain list")
+pth = raw_input("[*] Enter the path of the domain list - ")
 depot = DepotManager.get()
 driver = webdriver.PhantomJS()
 driver.set_window_size(800, 800) # set the window size that you need
@@ -9,7 +9,7 @@ with open(pth, "r") as f:
     for line in f.readlines():
         try:
             count = count + 1  
-            print 'Taking screenshot for site - '+line
+            print '[*] Taking screenshot for site - '+line
             driver.get(str(line.strip()))
             filename = line.replace('/','~')
             print filename
